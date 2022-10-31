5318face4aec23abaa8ecdf0cfe2a9d5f840da6b
Virkkunen <vrkknn@vrkknn.net>
2022-10-31 20:32:12 -0300

    early logic

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+const oneSecond = 1000;
+
 export default class Timer extends Component {
   
   state = {
@@ -7,13 +9,33 @@ export default class Timer extends Component {
     timerActive: false,
   }
 
+  presetTime = (e) => {
+    const time = Number(e.target.value);
+    this.setState({seconds: time});
+    console.log(this.state)
+  };
+
+  startTimer = () => {
+    this.setState({timerActive: true});
+    // o countdown em si
+    this.intervalId = setInterval(() => {
+      this.setState((prevState) => ({seconds: prevState.seconds - 1}));
+    }, oneSecond);
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
   clearState = () => {
+    this.componentWillUnmount();
     this.setState({
       seconds: 0,
       timerActive: false,
     });
     console.log('state cleared');
   };
+
 
   render() {
     const { timerActive, seconds } = this.state;
@@ -25,17 +47,17 @@ export default class Timer extends Component {
             <input type="number" name='custom-time' min='0' placeholder='Time'/>
           </label>
           <div className='presets'>
-            <button type='button' className='blue mono' name='5' value='300' disabled={timerActive}>5:00</button>
-            <button type='button' className='blue mono' name='10' value='600' disabled={timerActive}>10:00</button>
-            <button type='button' className='blue mono' name='15' value='900' disabled={timerActive}>15:00</button>
+            <button type='button' className='blue mono' name='5' value='300' disabled={timerActive} onClick={this.presetTime}>5:00</button>
+            <button type='button' className='blue mono' name='10' value='600' disabled={timerActive} onClick={this.presetTime}>10:00</button>
+            <button type='button' className='blue mono' name='15' value='900' disabled={timerActive} onClick={this.presetTime}>15:00</button>
           </div>
           <div className='control-buttons'>
             {!timerActive ? (
-              <button type='button' className='green sans'>Start</button>
+              <button type='button' className='green sans' onClick={this.startTimer}>Start</button>
             ) : (
               <button type='button' className='red sans'>Stop</button>
             )}
-            <button type='button' className='purple sans'>Reset</button>
+            <button type='button' className='purple sans' onClick={this.clearState}>Reset</button>
           </div>
         </div>
       </div>
