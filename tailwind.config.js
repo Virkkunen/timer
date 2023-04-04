b5014c642378fe5a7d903778c09d8c5f749a1ad1
Virkkunen <vrkknn@vrkknn.net>
2023-04-04 09:27:06 -0300

    refactor: vite, tsx, tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['"Fira Sans"', 'sans-serif'],
+      mono: ['Fira Code', 'monospace']
+    }
+  },
+  plugins: [
+    require('@catppuccin/tailwindcss')({
+      defaultFlavour: 'mocha'
+    }),
+  ],
+}
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { ghPages } from 'vite-plugin-gh-pages';
+
+// https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+  plugins: [react(), ghPages()],
+  base: '/timer/',
+  publicDir: 'public',
+  build: {
+    manifest: true,
+  }
+})
